c54aa818a8904df5ff6b079a88c7b68aa9669487
goldem64zyx <83962210+goldem64zyx@users.noreply.github.com>
2021-05-18 00:33:10 -0500

    Update success.php

--- codeingniter/app/Views/success.php
+++ codeingniter/app/Views/success.php
@@ -5,7 +5,7 @@
 <div class="container">
     <div class="row text-center">
         <div class="col-sm-6 col-sm-offset-3">
-        <br><br> <h2 style="color:#0fad00"><?php echo $user ?></h2>
+        <br><br> <h2 style="color:#0fad00"><?php echo $user_name ?></h2>
         </div>
     </div>
 </body>
