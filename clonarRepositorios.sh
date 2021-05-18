ea0e7593795ed0d115d522df3e7250c0823abd5a
goldem64zyx <goldem64@gmail.com>
2021-05-18 06:52:30 -0500

    script para clonar todos los repositorios de btc06

---
+++ clonarRepositorios.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/teslajuarez/btc6_alejandro-verdin.git
+git clone https://github.com/alan916/btc06_alan.git
+git clone https://github.com/Euan740/BTC6_Euan.git
+git clone https://github.com/diberoberto12/btc6_Roberto.git
+git clone https://github.com/sosaShutup/btc6_sosa.git
+git clone https://github.com/Jose-Antonio2003/btc6_Jose.git
+git clone https://github.com/osiris-cambara/btc6_OsirisCambara.git
+git clone https://github.com/SantiagoBtc6/Santiago-BTC6.git
+git clone https://github.com/Shayna27/Btc6_ShaynaRomero.git
+git clone https://github.com/MarioGarcia7w7/Btc6_MarioGarcia.git
+git clone https://github.com/JohanaJordanL/Btc6_JohanaJordan.git
